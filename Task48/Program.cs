910760d0d97a971c657b59551c0dfee162a7aae9
Dinis <Dinis_abd@example.com>
2022-08-25 21:59:06 +0300

    add task

---
+++ Task48/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // строки - row
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы- col
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            Console.Write(j < matrix.GetLength(1) - 1 ?
+            $"{matrix[i, j],4}," : $"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void MatrixSumIdx(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -9, 9);
+PrintMatrix(array2d);
+Console.WriteLine();
+MatrixSumIdx(array2d);
+PrintMatrix(array2d);
